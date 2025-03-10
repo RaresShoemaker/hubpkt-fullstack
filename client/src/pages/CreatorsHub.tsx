4d739523336d40a44ec5103d9943216674dfb19a
Rareș Ciubotaru <raresciubotaru@Raress-MacBook-Pro-2.local>
2025-03-10 22:29:10 +0200

    Remove redudant and faulty components and layouts

--- client/src/pages/CreatorsHub.tsx
+++ client/src/pages/CreatorsHub.tsx
@@ -1,49 +1,11 @@
-import React, { useEffect } from 'react';
-import CategoryOverviewLayout from '../layouts/CategoryOverviewLayout';
-import SEO from '../components/SEO';
-import CreatorsStackedLogo from '../assets/PktCreatorStackLogo.png';
-import { useCategories } from '../store/features/categories/useCategories';
-import { useCards } from '../store/features/cards/useCards';
-import CreatorsCategoryContainer from '../components/Creators/CreatorsContainer';
+import React from 'react';
 
-const CreatorsHubPage: React.FC = () => {
-  const { clientCategory } = useCategories();
-  const { handleFetchCreatorsCards, creatorsCards } = useCards();
-
-  useEffect(() => {
-    if (clientCategory) {
-      handleFetchCreatorsCards(clientCategory.id);
-    }
-  }, [handleFetchCreatorsCards, clientCategory]);
-
-  return (
-    <>
-      <SEO
-        title='Creator Hub'
-        description='Creator Hub'
-        keywords='Creator Hub'
-        url='https://hub.pkt.cash/creatorhub'
-        imgSrc={CreatorsStackedLogo}
-      />
-      <CategoryOverviewLayout>
-        <div className='flex flex-col gap-8'>
-          {creatorsCards && Object.keys(creatorsCards).length > 0 ? (
-            Object.entries(creatorsCards).map(([key, categoryData]) => (
-              categoryData && (
-                <CreatorsCategoryContainer 
-                  key={key}
-                  title={categoryData.title}
-                  data={categoryData.data}
-                />
-              )
-            ))
-          ) : (
-            <p className="text-white">Loading creator content...</p>
-          )}
+const HomePage:React.FC = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
         </div>
-      </CategoryOverviewLayout>
-    </>
-  );
-};
+    );
+} 
 
-export default CreatorsHubPage;
+export default HomePage;
