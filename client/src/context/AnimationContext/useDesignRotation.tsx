92bd4c8fd27e614e0ca2799d01682dc3188bf1d8
Rareș Ciubotaru <raresciubotaru@Raress-MacBook-Pro-2.local>
2025-04-08 18:57:18 +0300

    Remove unused hook

--- client/src/context/AnimationContext/useDesignRotation.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { DesignContext } from '../context/DesignContext';
-
-// Custom hook to use the design context
-export function useDesignRotation() {
-  return useContext(DesignContext);
-}
