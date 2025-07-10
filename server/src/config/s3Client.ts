65fa0650317f13ad3cb4c4d9c2e0c67d0792bc05
Rares Ciubotaru <47574885+RaresShoemaker@users.noreply.github.com>
2025-07-10 15:45:07 +0300

    Feature/change upload for localstorage filesystem (#17)
    * Refactor upload files logic
    
    * Solve build problems

--- server/src/config/s3Client.ts
+++ server/src/config/s3Client.ts
@@ -1,13 +1,13 @@
-import { S3Client } from "@aws-sdk/client-s3";
-import { config } from "./environment";
+// import { S3Client } from "@aws-sdk/client-s3";
+// import { config } from "./environment";
 
-const { R2_ACCOUNT_ID, R2_ACCESS_KEY_ID, R2_SECRET_ACCESS_KEY } = config;
+// const { R2_ACCOUNT_ID, R2_ACCESS_KEY_ID, R2_SECRET_ACCESS_KEY } = config;
 
-export const s3Client = new S3Client({
-  endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
-  credentials: {
-    accessKeyId: R2_ACCESS_KEY_ID,
-    secretAccessKey: R2_SECRET_ACCESS_KEY,
-  },
-  region: 'us-east-1',
-})
+// export const s3Client = new S3Client({
+//   endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+//   credentials: {
+//     accessKeyId: R2_ACCESS_KEY_ID,
+//     secretAccessKey: R2_SECRET_ACCESS_KEY,
+//   },
+//   region: 'us-east-1',
+// })
